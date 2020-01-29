26095bc592dc888e6acb5c83f2f90961766eac88
ramonigarciae <ramoni.garciae@gmail.com>
2020-01-28 22:56:24 -0600

    Versión inicial, plantilla del proyecto

---
+++ src/test/java/com/rg/fechas/fechas/FechasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rg.fechas.fechas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FechasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
